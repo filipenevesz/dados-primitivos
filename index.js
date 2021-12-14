1559f89d9dfa1e39d08daeb4c3dc3ff515b79727
Filipe <fedzn11@gmail.com>
2021-12-14 15:42:54 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// String, number , undefined , null , boolean
+const nome = 'Filipe'; //string
+const nome1 = "Filipe"; //string
+const nome2 = `Filipe`; //string
+const num1 = 10; //number
+const num2 = 10.52; //number
+let nomeAluno; //undefined -> não aponta pra local nenhuma na memória
+const sobrenomeAluno = null; //Nulo -> não aponta pra local nenhuma na memória
+const aprovado = true; // Boolean = true, false (lógico)
+
